cbac9398264c569d1b9f0a598c9922d10c183c50
jakehilborn <jakehilborn@gmail.com>
2020-05-03 12:29:44 -0700

    Factor burst_count into dynamic burst_min_gap calculation

--- src/debbit.py
+++ src/debbit.py
@@ -130,8 +130,9 @@ def get_burst_min_gap(merchant, cur_purchase_count, now):
 
     month_end_day = merchant.max_day or DAYS_IN_MONTH[now.month] - 1
     remaining_secs_in_month = (datetime(now.year, now.month, month_end_day) - now).total_seconds()
+    remaining_purchase_count = merchant.total_purchases - cur_purchase_count
 
-    dynamic_burst_min_gap = int(remaining_secs_in_month / (merchant.total_purchases - cur_purchase_count) / 5)
+    dynamic_burst_min_gap = int(remaining_secs_in_month / 4 / remaining_purchase_count * merchant.burst_count)
     default_burst_min_gap = 79200  # 22 hours
 
     return min(dynamic_burst_min_gap, default_burst_min_gap)
