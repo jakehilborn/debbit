01a0fbdd85cb2312cf1e6723d3a6c1f76438ce81
jakehilborn <jakehilborn@gmail.com>
2020-05-25 18:47:01 -0700

    Update example_merchant description

--- src/program-files/merchants/example_merchant.py
+++ src/program-files/merchants/example_merchant.py
@@ -16,9 +16,8 @@ How to add a new merchant module to debbit
 
 Create a new .py file in the merchants directory. Create a new block in config.txt such that the merchant name matches
 the name of your new file (excluding .py). The file must have a function with the signature
-`def web_automation(driver, merchant, amount):` that returns a `Result` in all possible scenarios. There is no
-Result.error enum since it is expected that your web automation will throw an exception in case something does cause
-an error.
+`def web_automation(driver, merchant, amount):` that returns a `Result` in all possible scenarios. In error scenarios you
+may return Result.failed or simply let whatever exception be thrown. It will be caught and handled correctly by debbit.py
 
 For more complex scenarios, please refer to the other merchant .py files.
 '''
@@ -33,11 +32,11 @@ def web_automation(driver, merchant, amount):
     )
 
     if not logged_in:
-        time.sleep(1)  # pause to let user watch what's happening - not necessary for real merchants
+        time.sleep(2)  # pause to let user watch what's happening - not necessary for real merchants
         driver.find_element_by_id('username').send_keys(merchant.usr)
-        time.sleep(1)  # pause to let user watch what's happening - not necessary for real merchants
+        time.sleep(2)  # pause to let user watch what's happening - not necessary for real merchants
         driver.find_element_by_id('password').send_keys(merchant.usr)
-        time.sleep(1)  # pause to let user watch what's happening - not necessary for real merchants
+        time.sleep(2)  # pause to let user watch what's happening - not necessary for real merchants
         driver.find_element_by_id('login').click()
         WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.ID, 'submit-payment')))
 
@@ -48,11 +47,11 @@ def web_automation(driver, merchant, amount):
     elif utils.str_to_cents(cur_balance) < amount:
         amount = utils.str_to_cents(cur_balance)
 
-    time.sleep(1)  # pause to let user watch what's happening - not necessary for real merchants
+    time.sleep(2)  # pause to let user watch what's happening - not necessary for real merchants
     driver.find_element_by_xpath("//*[contains(text(), 'card ending in " + merchant.card + "')]").click()
-    time.sleep(1)  # pause to let user watch what's happening - not necessary for real merchants
+    time.sleep(2)  # pause to let user watch what's happening - not necessary for real merchants
     driver.find_element_by_id('amount').send_keys(utils.cents_to_str(amount))
-    time.sleep(1)  # pause to let user watch what's happening - not necessary for real merchants
+    time.sleep(2)  # pause to let user watch what's happening - not necessary for real merchants
     driver.find_element_by_id('submit-payment').click()
 
     try:
@@ -61,5 +60,5 @@ def web_automation(driver, merchant, amount):
         return Result.unverified  # Purchase command was executed, yet we are unable to verify that it was successfully executed.
         # since debbit may have spent money but isn't sure, we log the error and stop any further payments for this merchant until the user intervenes
 
-    time.sleep(3)  # sleep for a bit to show user that payment screen is reached
+    time.sleep(5)  # sleep for a bit to show user that payment screen is reached
     return Result.success
