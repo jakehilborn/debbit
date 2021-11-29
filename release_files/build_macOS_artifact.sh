af25bfe3dd4695a42f3f274ea3d7cae3ce0c77fd
Jake Hilborn <jakehilborn@gmail.com>
2021-11-28 21:17:35 -0800

    Release scripts (#76)

---
+++ release_files/build_macOS_artifact.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+ROOT=$(dirname $(cd "$(dirname "$0")"; pwd -P))
+
+# pyinstaller uses globally installed pip packages instead of pipenv packages
+cd "$ROOT/src"
+pip3 install -U pip
+pip3 list --outdated --format=freeze | cut -d' ' -f1 | xargs -n1 pip3 install -U
+[ -z $(pip3 list --outdated) ] # exit script if there are still outdated packages
+pip3 install -Iv coverage==5.3.1
+
+if [[ $(sw_vers -productVersion) == "10.12"* ]]; then
+    echo "downgrading pyinstaller to work on 10.12"
+    pip3 install -Iv pyinstaller==4.3 # workaround "Executable contains code signature" bug on macOS 10.12
+fi
+
+find "$ROOT/src" -name "__pycache__" -print0 | xargs -0 rm -rf
+rm -rf "$ROOT/src/build"
+rm -rf "$ROOT/src/dist"
+
+pyinstaller --clean -F -c debbit.py program_files/merchants/*.py
+cp dist/debbit release/macOS
+
+cd "$ROOT/src/release"
+REL_VERSION=$(cat rel_version.txt)
+mv macOS "debbit-$REL_VERSION-macOS"
+zip -r "debbit-$REL_VERSION-macOS.zip" "debbit-$REL_VERSION-macOS"
---
+++ release_files/prep_artifact_content.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+ROOT=$(dirname $(cd "$(dirname "$0")"; pwd -P))
+
+cd "$ROOT/src"
+rm -rf release
+mkdir -p release/common
+
+REL_VERSION=$(grep "VERSION = " "$ROOT/src/debbit.py" | cut -d'=' -f2 | tr -d "[' ]")
+echo $REL_VERSION > release/rel_version.txt
+tail -n +2 sample_config.txt > release/common/config.txt # the tail command removes the first line from the file
+cp "$ROOT/release_files/INSTRUCTIONS.html" release/common
+cp -a program_files release/common/program_files
+cp "$ROOT/release_files/HOW_TO_EDIT_MERCHANTS.txt" release/common/program_files/merchants
+
+cd "$ROOT/src/release"
+
+# remove non source files
+rm -rf common/program_files/cookies
+rm -rf common/program_files/*.log
+find "$ROOT/src" -name "__pycache__" -print0 | xargs -0 rm -rf
+find "$ROOT/src" -name ".DS_Store" -delete
+rm -rf "$ROOT/src/build"
+rm -rf "$ROOT/src/dist"
+rm -f "$ROOT/src/debbit.spec"
+
+cp -a common macOS
+cp -a common win64
+
+# extract v0.30.0 from <html><body>You are being <a href="https://github.com/mozilla/geckodriver/releases/tag/v0.30.0">redirected</a>.</body></html>%
+GECKODRIVER_VERSION=$(curl https://github.com/mozilla/geckodriver/releases/latest | sed 's|.*tag/\(.*\)\".*|\1|')
+
+# macOS specific files
+curl -LJO https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-macos.tar.gz
+tar -xzf geckodriver-*-macos.tar.gz
+mv geckodriver macOS/program_files
+
+# win64 specific files
+curl -LJO https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-win64.zip
+unzip geckodriver-*-win64.zip
+mv geckodriver.exe win64/program_files
+cp "$ROOT/release_files/debbit_keep_window_open.bat" win64
+unix2dos win64/config.txt # if we don't do this, Notepad on Windows 7 does not show new lines
+
+# specific to author's dev env
+rm -rf "$HOME/VirtualBox VMs/shared/debbit"
+mkdir -p "$HOME/VirtualBox VMs/shared/debbit"
+cp -a "$ROOT/release_files" "$HOME/VirtualBox VMs/shared/debbit"
+cp -a "$ROOT/src" "$HOME/VirtualBox VMs/shared/debbit"
+
+rm -rf "$HOME/VirtualBox VMs/shared_mac/Dropbox/debbit"
+mkdir -p "$HOME/VirtualBox VMs/shared_mac/Dropbox/debbit"
+cp -a "$ROOT/release_files" "$HOME/VirtualBox VMs/shared_mac/Dropbox/debbit"
+cp -a "$ROOT/src" "$HOME/VirtualBox VMs/shared_mac/Dropbox/debbit"
