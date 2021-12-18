6b5108650f03ea971bad18dd1eb003ce23e68dfa
Jake Hilborn <jakehilborn@gmail.com>
2021-12-17 18:11:04 -0800

    set development version v2.1.5-dev

--- src/debbit.py
+++ src/debbit.py
@@ -750,7 +750,7 @@ if __name__ == '__main__':
     STATE_WRITE_LOCK = Lock()
     WEB_DRIVER_LOCK = Lock()
     DAYS_IN_MONTH = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-    VERSION = 'v2.1.4'
+    VERSION = 'v2.1.5-dev'
     VERSION_INT = 10
 
     LOGGER.info('       __     __    __    _ __ ')
