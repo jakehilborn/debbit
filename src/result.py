6843cae3fbd74abbc21f1316a187cdb90e255e24
Jake Hilborn <jakehilborn@gmail.com>
2021-12-28 22:29:00 -0800

    Add comments to result.py

--- src/result.py
+++ src/result.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class Result(Enum):
     success = 'success',
-    skipped = 'skipped',
-    unverified = 'unverified',
-    failed = 'failed'
+    skipped = 'skipped',  # try again 24 hours later, do not notify user
+    unverified = 'unverified',  # do not try again, notify user of error
+    failed = 'failed'  # retry up to 5 times, if still failing then notify user of error after final failure
